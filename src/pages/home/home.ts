6e2ec7a37e95196c04db9e0f24fe79e830872ff6
Vane <lyssrc@gmail.com>
2019-10-16 20:19:10 -0500

    Ya jalan las alertas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Alert, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,11 +10,17 @@ export class HomePage {
   nombre = '';
 
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+              public alertCtrl: AlertController) {
 
   }
 
   click(){
-    console.log('Hola', this.nombre);
+    //console.log('Hola', this.nombre);
+    const alerta = this.alertCtrl.create({
+      title: 'Hola', subTitle: this.nombre, buttons: ['Jalo', 'No jalo']
+    });
+
+    alerta.present();
   }
 }
